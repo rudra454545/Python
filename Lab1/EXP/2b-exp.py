32aafafea00ccada9df34f15f2db7145f902774a
rudra454545 <157367462+rudra454545@users.noreply.github.com>
2025-12-26 23:41:58 +0530

    Update

--- Lab1/EXP/2b-exp.py
+++ Lab1/EXP/2b-exp.py
@@ -1,1 +1,14 @@
-#b)Write a program to check if the string is a palindrome or not
+def check_palindrome():
+    original_input = input("Enter a word or phrase: ")
+    
+    # Cleaning the string by removing spaces and making it lowercase
+    cleaned_string = original_input.replace(" ", "").lower()
+    
+    # Comparing the string with its reverse
+    if cleaned_string == cleaned_string[::-1]:
+        print(f"Yes! '{original_input}' is palindrome.")
+    else:
+        print(f"No. '{original_input}' is not a palindrome.")
+
+# Calling the function
+check_palindrome()
