9fd59d57e811c2e3340441cfd47b12a7abbcb697
rudra454545 <157367462+rudra454545@users.noreply.github.com>
2025-12-27 10:36:42 +0530

    Update: Add all remaining files

--- Lab1/EXP/3b-exp.py
+++ Lab1/EXP/3b-exp.py
@@ -1,1 +1,18 @@
 #3b) Write a program that takes two lists and returns True if they are equal, otherwise False
+def check_lists():
+    
+    list1 = input("Enter elements of the first list (separated by space): ").split()
+    list2 = input("Enter elements of the second list (separated by space): ").split()
+
+    print(f"\nList 1: {list1}")
+    print(f"List 2: {list2}")
+
+   
+    if list1 == list2:
+        return True
+    else:
+        return False
+
+
+result = check_lists()
+print(f"Are the lists equal? {result}")
